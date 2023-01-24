54df1b15d65a2125ff1e65601592f32170f202cc
gabriel-santosdev <gabrieljorgesilvasantos@outlook.com>
2023-01-24 12:34:13 -0300

    add page home complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
+import Container from './components/layout/Container';
+import Navbar  from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <p>Rosc</p>
-      <h2>Teste Teste</h2>
-    </div>
+    <Router>
+        <Navbar/>
+          <Switch>
+          <Container customClass="min-height">
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route  path="/projects">
+              <Projects />
+            </Route>
+            <Route  path="/company">
+              <Company />
+            </Route>
+            <Route  path="/contact">
+              <Contact />
+            </Route>
+            <Route  path="/newproject">
+              <NewProject />
+            </Route>
+          </Container>
+          </Switch>
+        <Footer/>
+    </Router>
   );
 }
 
