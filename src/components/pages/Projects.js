4947a3447d249921a08e199e1049bc964290cff5
gabriel-santosdev <gabrieljorgesilvasantos@outlook.com>
2023-01-25 15:20:57 -0300

    add message component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,19 @@
+import { useLocation } from 'react-router-dom'
+import Message from '../layout/Message'
+
 function Projects() {
+
+    const location = useLocation()
+    let message = ''
+    if(location.state) {
+        message = location.state.message
+    }
+
     return(
-        <h1>Projects</h1>
+        <div>
+            <h1>Meus Projetos</h1>
+            {message && <Message  type="success" msg={message} />}
+        </div>
     )
 }
 
