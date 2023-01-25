31fabe15ac64ef98e0279ed80ebacfc00cf8b806
gabriel-santosdev <gabrieljorgesilvasantos@outlook.com>
2023-01-25 13:07:37 -0300

    add new configuration at form and add API with JSON server

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -5,8 +5,18 @@ function Select({text, name, options, handleOnChange, value}){
     return(
         <div className={styles.form_control}>
             <label htmlfor={name}>{text}</label>
-            <select name={name} id={name}>
+            <select 
+            name={name} 
+            id={name} 
+            onChange={handleOnChange} 
+            value={value || ''}
+            >
                 <option>Selecione uma opção</option>
+                {options.map((option) => (
+                        <option value={option.id} key={option.id}>
+                            {option.name}
+                        </option>
+                ))}
             </select>
         </div>
     )
